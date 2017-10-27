69057e575a2de6bb55832143b767e3c766f3deb5
Christopher Chen <christopher.chen1124@gmail.com>
2017-10-27 12:37:14 -0700

    Committing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,12 @@
+var path = require("path");
 module.exports=function(app)
 {
 	// Basic route that sends the user first to the AJAX Page
-	app.get("/", function(req, res) {
-	  res.sendFile(path.join(__dirname, "home.html"));
+	app.get("/home", function(req, res) {
+	  res.sendFile(path.join(__dirname, "../data/home.html"));
 	});
 
 	app.get("/survey", function(req, res) {
-	  res.sendFile(path.join(__dirname, "survey.html"));
+	  res.sendFile(path.join(__dirname, "../data/survey.html"));
 	});	
 }
