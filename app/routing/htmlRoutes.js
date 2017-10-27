627b5179035599347baca164377fe451c38f4082
Christopher Chen <christopher.chen1124@gmail.com>
2017-10-27 12:47:39 -0700

    changed html route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,10 @@
 var path = require("path");
 module.exports=function(app)
 {
+	app.get("/", function(req, res) {
+	  res.sendFile(path.join(__dirname, "../data/home.html"));
+	});
+	
 	// Basic route that sends the user first to the AJAX Page
 	app.get("/home", function(req, res) {
 	  res.sendFile(path.join(__dirname, "../data/home.html"));
